73e3a64c0ec41f4b760e470e829dd46d0cc3c404
Seasupermike <mkiwa37@gmail.com>
2025-09-02 17:00:46 -0400

    Update script.js

--- Global/script.js
+++ Global/script.js
@@ -1,11 +1,10 @@
 // JavaScript source code
-const orgName = "The Watchers";
-
-const logotypes = document.querySelectorAll("logotype");
-
-for (let i = 0; i < logotypes.length; i++) {
-    let orgNameText = document.createElement("h1");
-    orgNameText.textContent = orgName;
-    orgNameText.classList.add("logotype");
-    logotypes[i].appendChild(orgNameText);
-}
+prefab("logotype", [
+    {
+        Name: "h1",
+        text: "The Watchers",
+        classes: [
+            "logotype"
+        ],
+    }
+])
